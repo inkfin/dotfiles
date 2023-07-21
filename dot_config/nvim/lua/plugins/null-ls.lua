e1fb6015b406e884b523feada6f81b6add97357f
inkfin <inkfinite@qq.com>
2023-07-22 03:33:55 +0800

    fix windows clang-check error (.clang-format encoding error!)

--- dot_config/nvim/lua/plugins/null-ls.lua
+++ dot_config/nvim/lua/plugins/null-ls.lua
@@ -2,20 +2,22 @@ return {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function()
         local nls = require("null-ls")
-        local formatting = require("null-ls").builtins.formatting
         return {
             root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
             sources = {
-                formatting.stylua,
-                formatting.shfmt,
-                formatting.prettier.with({
+                nls.builtins.formatting.stylua,
+                nls.builtins.formatting.shfmt,
+                nls.builtins.formatting.clang_format,
+                nls.builtins.formatting.rustfmt,
+                nls.builtins.formatting.prettier.with({
                     disabled_filetypes = { "markdown", "typescript" },
                     timeout = 30000,
                 }),
-                formatting.markdownlint.with({
+                nls.builtins.formatting.markdownlint.with({
                     timeout = 30000,
                 }),
             },
+            -- debug = true,
         }
     end,
 }
