4017fc8a73129288a5ad6a1ec3c331babaaad166
inkfin <inkfinite@qq.com>
2023-09-05 15:21:31 -0400

    add neovide options

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -146,4 +146,47 @@ elseif vim.fn.has("win32") then
     vim.go.shellxquote = ""
 end
 
+--- Neovide Configurations
+if vim.g.neovide then
+    vim.o.guifont = "FiraCode Nerd Font Mono:h16"
+    vim.o.linespace = 0 -- Controls spacing between lines, may also be negative.
+    vim.o.neovide_scale_factor = 1.0
+
+    -- Background Color (Currently macOS only)
+    if vim.fn.has("mac") == 1 then
+        local alpha = function()
+            return string.format("%x", math.floor((255 * vim.g.transparency) or 0.8))
+        end
+        -- g:neovide_transparency should be 0 if you want to unify transparency of content and title bar.
+        vim.g.neovide_transparency = 0.0
+        vim.g.transparency = 0.8
+        vim.g.neovide_background_color = "#0f1117" .. alpha()
+    else
+        vim.g.neovide_transparency = 0.8
+    end
+    vim.g.neovide_theme = "auto"
+
+    -- Windows configuration
+    if vim.fn.has("win32") == 1 then
+        vim.g.neovide_fullscreen = true
+        vim.g.neovide_padding_top = 200
+        vim.g.neovide_padding_bottom = 200
+        vim.g.neovide_padding_right = 200
+        vim.g.neovide_padding_left = 200
+    end
+
+    -- Floating Blur Amount
+    vim.g.neovide_floating_blur_amount_x = 2.0
+    vim.g.neovide_floating_blur_amount_y = 2.0
+
+    vim.g.neovide_hide_mouse_when_typing = true
+    vim.g.neovide_underline_automatic_scaling = true
+    vim.g.neovide_confirm_quit = true
+    vim.g.neovide_input_macos_alt_is_meta = true
+
+    -- Animations
+    vim.g.neovide_scroll_animation_length = 0.3
+    vim.g.neovide_cursor_vfx_mode = "sonicboom"
+end
+
 require("utils.func")
