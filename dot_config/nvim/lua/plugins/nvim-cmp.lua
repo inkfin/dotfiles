20780fad4c08bb9621353119d396671cac28da52
inkfin <inkfinite@qq.com>
2023-12-12 19:25:28 -0500

    add nvim dictionary

--- dot_config/nvim/lua/plugins/nvim-cmp.lua
+++ dot_config/nvim/lua/plugins/nvim-cmp.lua
@@ -13,11 +13,23 @@ return {
     {
         "hrsh7th/nvim-cmp",
         dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-emoji",
-            "saadparwaiz1/cmp_luasnip",
+            { "hrsh7th/cmp-emoji" },
+            {
+                "uga-rosa/cmp-dictionary",
+                opts = function(_, opts)
+                    local dict = require("cmp_dictionary")
+
+                    opts.exact = 2
+                    opts.capacity = 5
+                    opts.max_items = -1
+
+                    dict.switcher({
+                        spelllang = {
+                            en = vim.fn.stdpath("config") .. "/spell/en.dict",
+                        },
+                    })
+                end,
+            },
         },
         opts = function(_, opts)
             local has_words_before = function()
@@ -64,12 +76,36 @@ return {
                     end
                 end, { "i", "s" }),
             })
-            opts.sources = {
+            opts.sources = cmp.config.sources({
                 { name = "nvim_lsp" },
+                { name = "emoji" },
                 { name = "luasnip" },
-                { name = "buffer" },
                 { name = "path" },
                 { name = "copilot" },
+                { name = "buffer" },
+            }, {
+                { name = "buffer" },
+                { name = "dictionary", keyword_length = 2 },
+            })
+            opts.formatting = {
+                format = function(entry, item)
+                    local icons = require("lazyvim.config").icons.kinds
+                    if icons[item.kind] then
+                        item.kind = icons[item.kind] .. item.kind
+                    end
+                    -- item.menu = ({
+                    --     nvim_lsp = "[LSP]",
+                    --     emoji = "[Emoji]",
+                    --     luasnip = "[LuaSnip]",
+                    --     nvim_lua = "[Lua]",
+                    --     latex_symbols = "[LaTeX]",
+                    --     path = "[Path]",
+                    --     copilot = "[AI]",
+                    --     buffer = "[Buffer]",
+                    --     dictionary = "[Dict]",
+                    -- })[entry.source.name]
+                    return item
+                end,
             }
         end,
     },
