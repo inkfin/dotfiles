a025907debe5eadbdcf0cf3ceb0f1afcaf485a2d
inkfin <inkfinite@qq.com>
2023-12-13 01:45:05 -0500

    fix markdown config

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -1,3 +1,5 @@
+-- not using lazyvim's language preset, control plugins by ourselves
+
 return {
     {
         "iamcco/markdown-preview.nvim",
@@ -34,6 +36,54 @@ return {
                     \ 'toc': {}
                     \ }
             ]])
+        end,
+    },
+    {
+        "lukas-reineke/headlines.nvim",
+        opts = function()
+            local opts = {}
+            for _, ft in ipairs({ "markdown", "norg", "rmd", "org" }) do
+                opts[ft] = {
+                    headline_highlights = {},
+                }
+                for i = 1, 6 do
+                    local hl = "Headline" .. i
+                    vim.api.nvim_set_hl(0, hl, { link = "Headline", default = true })
+                    table.insert(opts[ft].headline_highlights, hl)
+                end
+            end
+            return opts
+        end,
+        ft = { "markdown", "norg", "rmd", "org" },
+        config = function(_, opts)
+            -- PERF: schedule to prevent headlines slowing down opening a file
+            vim.schedule(function()
+                require("headlines").setup(opts)
+                require("headlines").refresh()
+            end)
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = function(_, opts)
+            if type(opts.ensure_installed) == "table" then
+                vim.list_extend(opts.ensure_installed, { "markdown", "markdown_inline" })
+            end
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                marksman = {},
+            },
+        },
+    },
+    {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+            opts.ensure_installed = opts.ensure_installed or {}
+            vim.list_extend(opts.ensure_installed, { "marksman" })
         end,
     },
 }
