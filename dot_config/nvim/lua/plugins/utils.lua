8f9edddd5d6a6b27b35fb806d35fff86d43f7752
inkfin <inkfinite@qq.com>
2023-12-18 12:49:24 -0500

    add download im-select script

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -18,6 +18,27 @@ return {
     {
         "keaising/im-select.nvim",
         config = function()
+            -- download im-select executable if don't exists
+            if vim.fn.executable("im-select") == 0 then
+                local cmd = ""
+                if vim.fn.has("win32") == 1 then
+                    cmd = [[
+                        curl -L https://github.com/daipeihust/im-select/raw/master/win/out/x86/im-select.exe -o $HOME\.local\bin\im-select.exe
+                    ]]
+                elseif vim.fn.has("mac") == 1 then
+                    if vim.fn.executable("brew") == 1 then
+                        cmd = [[
+                            brew tap daipeihust/tap
+                            brew install im-select
+                        ]]
+                    else
+                        cmd = [[
+                            curl -Ls https://raw.githubusercontent.com/daipeihust/im-select/master/install_mac.sh | sh
+                        ]]
+                    end
+                end
+                os.execute(cmd)
+            end
             require("im_select").setup({
                 -- Async run `default_command` to switch IM or not
                 async_switch_im = false,
