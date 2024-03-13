5b779aa801c5cd812aee5c558afc0333bb711b8f
inkfin <inkfinite@qq.com>
2024-03-12 20:57:39 -0400

    fix nvim rust problem

--- dot_config/nvim/lua/plugins/rust.lua
+++ dot_config/nvim/lua/plugins/rust.lua
@@ -8,14 +8,7 @@ return {
             servers = {
                 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
                 -- Ensure mason installs the server
-                rust_analyzer = {
-                    keys = {
-                        { "<Leader>h", "<cmd>RustHoverActions<cr>", desc = "Hover Actions (Rust)" },
-                        { "<leader>cR", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
-                        { "<leader>dr", "<cmd>RustDebuggables<cr>", desc = "Run Debuggables (Rust)" },
-                        { "K", false },
-                    },
-                },
+                rust_analyzer = {},
                 taplo = {
                     keys = {
                         {
@@ -36,4 +29,24 @@ return {
         },
     },
     -- rustfmt: https://rust-lang.github.io/rustfmt/
+    -- rustaceanvim: https://github.com/mrcjkb/rustaceanvim
+    {
+        "mrcjkb/rustaceanvim",
+        opts = {
+            server = {
+                on_attach = function(_, bufnr)
+                    vim.keymap.set("n", "<leader>h", function()
+                        vim.cmd.RustLsp({ "hover", "actions" })
+                    end, { desc = "Hover Actions (Rust)", buffer = bufnr })
+
+                    vim.keymap.set("n", "<leader>ca", function()
+                        vim.cmd.RustLsp("codeAction")
+                    end, { desc = "Code Action", buffer = bufnr })
+                    vim.keymap.set("n", "<leader>dr", function()
+                        vim.cmd.RustLsp("debuggables")
+                    end, { desc = "Rust debuggables", buffer = bufnr })
+                end,
+            },
+        },
+    },
 }
