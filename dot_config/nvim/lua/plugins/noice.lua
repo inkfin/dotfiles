7d5089886ddc99f3d2517130af3114fd534733bd
inkfin <inkfinite@qq.com>
2024-03-17 15:42:34 -0400

    update noice setting

--- dot_config/nvim/lua/plugins/noice.lua
+++ dot_config/nvim/lua/plugins/noice.lua
@@ -1,60 +1,71 @@
 -- Plugin that show messages in floating flexible windows
 
 return {
-    "folke/noice.nvim",
-    enabled = not vim.g.started_by_firenvim,
-    opts = {
-        views = {
-            cmdline_popup = {
-                position = {
-                    row = -2,
-                    col = "50%",
+    {
+        "folke/noice.nvim",
+        enabled = not vim.g.started_by_firenvim,
+        opts = {
+            views = {
+                cmdline_popup = {
+                    position = {
+                        row = -2,
+                        col = "50%",
+                    },
+                },
+                cmdline_popupmenu = {
+                    position = {
+                        row = -5,
+                        col = "50%",
+                    },
                 },
             },
-            cmdline_popupmenu = {
-                position = {
-                    row = -5,
-                    col = "50%",
+            preset = {
+                cmdline_output_to_split = true,
+            },
+            -- disable some messages
+            routes = {
+                {
+                    skip = true,
+                    filter = { event = "msg_show", find = ":!chezmoi" },
                 },
             },
         },
-        preset = {
-            cmdline_output_to_split = true,
-        },
-        -- disable some messages
-        routes = {
+        keys = {
+            -- change scrolling keymap to <C-d/u> (can affect LSP)
             {
-                skip = true,
-                filter = { event = "msg_show", find = ":!chezmoi" },
+                "<C-d>",
+                function()
+                    if not require("noice.lsp").scroll(4) then
+                        return "<C-d>"
+                    end
+                end,
+                silent = true,
+                expr = true,
+                desc = "Scroll forward",
+                mode = { "i", "n", "s" },
             },
+            {
+                "<C-u>",
+                function()
+                    if not require("noice.lsp").scroll(-4) then
+                        return "<C-u>"
+                    end
+                end,
+                silent = true,
+                expr = true,
+                desc = "Scroll backward",
+                mode = { "i", "n", "s" },
+            },
+            { "<leader>fN", "<Cmd>Noice telescope<CR>", silent = true, desc = "Noice messages" },
         },
     },
-    keys = {
-        -- change scrolling keymap to <C-d/u> (can affect LSP)
-        {
-            "<C-d>",
-            function()
-                if not require("noice.lsp").scroll(4) then
-                    return "<C-d>"
-                end
-            end,
-            silent = true,
-            expr = true,
-            desc = "Scroll forward",
-            mode = { "i", "n", "s" },
+    {
+        "rcarriga/nvim-notify",
+        opts = {
+            fps = 60,
+            timeout = 3500,
+            render = "wrapped-compact", -- default, minimal, simple, compact, wrapped-compact
+            stages = "fade", -- fade_in_slide_out, fade, slide, static
         },
-        {
-            "<C-u>",
-            function()
-                if not require("noice.lsp").scroll(-4) then
-                    return "<C-u>"
-                end
-            end,
-            silent = true,
-            expr = true,
-            desc = "Scroll backward",
-            mode = { "i", "n", "s" },
-        },
-        { "<leader>fN", "<Cmd>Noice telescope<CR>", silent = true, desc = "Noice messages" },
     },
 }
