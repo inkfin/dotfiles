a459a111aedf91d576507aba03bf7905543fcfc9
inkfin <inkfinite@qq.com>
2024-07-06 12:49:22 +0800

    update latex template

---
+++ scripts/auto-install-latex.ps1
@@ -0,0 +1,18 @@
+# install pandoc for exporting
+scoop install pandoc
+
+# prefer miktex to texlive
+scoop install miktex
+# IMPORTANT: Remember to open miktex-console to
+#     apply update & set auto-install !!!
+
+## Templates
+# Eisvogel
+#   [https://github.com/Wandmalfarbe/pandoc-latex-template]
+#
+## Some cool fonts
+#
+# Chinese
+#   - Smiley-Sans
+#     [https://github.com/atelier-anchor/smiley-sans/releases/latest]
+#
