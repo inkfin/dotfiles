7898f3f7eaed5957d2e4f0fc381805e5c4dea036
inkfin <inkfinite@qq.com>
2024-07-26 11:44:24 +0800

    update linux setup scripts

---
+++ scripts/env_init/executable_auto-install-latex.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+. ~/scripts/env_init/helpers.sh
+
+if ! is_command neovim; then
+    echo "==========================================="
+    echo "======= installing pandoc & miktex ========"
+    echo "==========================================="
+
+    if ! is_command brew; then
+        echo "[ERROR]: can't find homebrew in path"
+        read -n1 -p "> should we try install brew now? : [y/n]" answer
+        case $answer in
+        Y | y)
+            echo "> running ~/.scripts/env_init/auto-install-homebrew.sh"
+            /bin/bash $HOME/scripts/env_init/auto-install-homebrew.sh
+            ;;
+        N | n)
+            echo "ok, good bye"
+            exit
+            ;;
+        *)
+            echo "ok, good bye"
+            exit
+            ;;
+        esac
+    fi
+
+    # install pandoc for exporting
+    brew_install_if_not_exist pandoc
+
+    if [[ $(uname -s) == "Linux" ]]; then
+        echo "No package for current system; check <https://miktex.org/howto/install-miktex-unx>"
+    else
+        # prefer miktex to texlive
+        brew_install_if_not_exist miktex
+    fi
+fi
+
+# IMPORTANT: Remember to open miktex-console to
+#     apply update & set auto-install !!!
+
+## Templates
+# Eisvogel
+#   [https://github.com/Wandmalfarbe/pandoc-latex-template]
+#
+## Some cool fonts
+#
+# Chinese
+#   - Smiley-Sans
+#     [https://github.com/atelier-anchor/smiley-sans/releases/latest]
+#
