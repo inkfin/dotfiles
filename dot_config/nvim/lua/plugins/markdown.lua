e6e3eb1a32d1968ac8cc409a0e03ebb4a9bd5253
inkfin <inkfinite@qq.com>
2024-07-27 14:56:23 +0800

    disable markdown headline plug due to lazyvim update

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -37,32 +37,32 @@ return {
         end,
     },
 
-    {
-        "lukas-reineke/headlines.nvim",
-        opts = function()
-            local opts = {}
-            for _, ft in ipairs({ "markdown", "quarto", "norg", "rmd", "org" }) do
-                opts[ft] = {
-                    headline_highlights = {},
-                    fat_headline_lower_string = "▀",
-                }
-                for i = 1, 6 do
-                    local hl = "Headline" .. i
-                    vim.api.nvim_set_hl(0, hl, { link = "Headline", default = true })
-                    table.insert(opts[ft].headline_highlights, hl)
-                end
-            end
-            return opts
-        end,
-        ft = { "markdown", "quarto", "norg", "rmd", "org" },
-        config = function(_, opts)
-            -- PERF: schedule to prevent headlines slowing down opening a file
-            vim.schedule(function()
-                require("headlines").setup(opts)
-                require("headlines").refresh()
-            end)
-        end,
-    },
+    -- {
+    --     "lukas-reineke/headlines.nvim",
+    --     opts = function()
+    --         local opts = {}
+    --         for _, ft in ipairs({ "markdown", "quarto", "norg", "rmd", "org" }) do
+    --             opts[ft] = {
+    --                 headline_highlights = {},
+    --                 fat_headline_lower_string = "▀",
+    --             }
+    --             for i = 1, 6 do
+    --                 local hl = "Headline" .. i
+    --                 vim.api.nvim_set_hl(0, hl, { link = "Headline", default = true })
+    --                 table.insert(opts[ft].headline_highlights, hl)
+    --             end
+    --         end
+    --         return opts
+    --     end,
+    --     ft = { "markdown", "quarto", "norg", "rmd", "org" },
+    --     config = function(_, opts)
+    --         -- PERF: schedule to prevent headlines slowing down opening a file
+    --         vim.schedule(function()
+    --             require("headlines").setup(opts)
+    --             require("headlines").refresh()
+    --         end)
+    --     end,
+    -- },
 
     -- paste an image to markdown from the clipboard
     -- :PasteImg,
