9a7051cc40d80e96b141cb5d030174f92313c696
inkfin <inkfinite@qq.com>
2025-02-05 17:32:13 -0500

    mod(nvim): remove annoying scroll

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -23,6 +23,13 @@ return {
         },
     },
     {
+        "folke/snacks.nvim",
+        opts = {
+            scroll = { enabled = false },
+            zen = { enabled = false },
+        },
+    },
+    {
         "hedyhli/outline.nvim",
         keys = { { "<leader>cs", false }, { "<leader>oo", "<cmd>Outline<cr>", desc = "Toggle Outline" } },
     },
