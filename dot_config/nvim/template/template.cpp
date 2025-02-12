6e49ac15512d55e8d14caedb43b1f2e980d2707b
inkfin <inkfinite@qq.com>
2025-02-11 19:12:38 -0500

    mod(nvim): cmakelists
    mod(nvim): cpp template

--- dot_config/nvim/template/template.cpp
+++ dot_config/nvim/template/template.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
-#include <algorithm>
-#include <queue>
-#include <cstring>
-#include <cmath>
-using namespace std;
 
 int main() {
-    cout << "Hello world!" << endl;
+    std::cout << "Hello world!" << std::endl;
 }
