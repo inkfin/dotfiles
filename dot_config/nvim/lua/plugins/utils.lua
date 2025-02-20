d361ac714fcbf961ee2b477908c305b11c8a3714
inkfin <inkfinite@qq.com>
2025-02-19 23:48:23 -0500

    mod(nvim): add yazi.nvim

--- dot_config/nvim/lua/plugins/utils.lua
+++ dot_config/nvim/lua/plugins/utils.lua
@@ -30,6 +30,28 @@ return {
         },
     },
     {
+        "mikavilpas/yazi.nvim",
+        keys = {
+            {
+                "<space>yy",
+                "<cmd>Yazi<cr>",
+                desc = "Open yazi (Directory of Current File)",
+                mode = { "n", "v" },
+            },
+        },
+        ---@type YaziConfig
+        opts = {
+            integrations = {
+                grep_in_directory = function(dir)
+                    Snacks.picker.grep({ dirs = { dir } })
+                end,
+                grep_in_selected_files = function(_, dirs)
+                    Snacks.picker.grep({ dirs = dirs })
+                end,
+            },
+        },
+    },
+    {
         "hedyhli/outline.nvim",
         keys = { { "<leader>cs", false }, { "<leader>oo", "<cmd>Outline<cr>", desc = "Toggle Outline" } },
     },
