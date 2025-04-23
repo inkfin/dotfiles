0c57a44be582aa643d459d3c4cc60f09028358c5
inkfin <inkfinite@qq.com>
2025-04-22 22:20:50 -0400

    mod(scripts/cmake): update use_googletest, improve

--- scripts/cmake/use_googletest.cmake
+++ scripts/cmake/use_googletest.cmake
@@ -10,8 +10,9 @@ FetchContent_Declare(
     googletest
     GIT_REPOSITORY  https://github.com/google/googletest.git
     GIT_TAG         6910c9d9165801d8827d628cb72eb7ea9dd538c5 # release-1.16.0
+    GIT_SHALLOW     TRUE
+    GIT_PROGRESS    TRUE
     SOURCE_DIR      ${CMAKE_SOURCE_DIR}/third_party/googletest
-    GIT_PROGRESS TRUE
     FIND_PACKAGE_ARGS NAMES GTest CONFIG REQUIRED
 )
 FetchContent_MakeAvailable(googletest)
@@ -22,3 +23,26 @@ else()
     message(STATUS "🖥 Using GoogleTest from system")
 endif()
 
+
+## Configuration
+
+enable_testing()
+
+if(WIN32)
+    set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+endif()
+
+# add_library(${BINARY_NAME}_lib STATIC ${SOURCE_FILES}) # Apps source files
+
+# file(GLOB_RECURSE TEST_FILES CONFIGURE_DEPENDS
+#     LIST_DIRECTORIES false
+#     test/*.cpp test/*.h
+# )
+
+# add_executable(gtest ${TEST_FILES})
+
+# target_link_libraries(gtest PRIVATE ${BINARY_NAME}_lib
+#     GTest::gtest GTest::gtest_main GTest::gmock GTest::gmock_main)
+
+# include(GoogleTest)
+# gtest_discover_tests(gtest)
