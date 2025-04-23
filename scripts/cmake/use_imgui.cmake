2b45bb92d522d82dab6307bfc47a4508d5daa92f
inkfin <inkfinite@qq.com>
2025-04-23 03:48:32 -0400

    mod(scripts/cmake): fix logic

---
+++ scripts/cmake/use_imgui.cmake
@@ -0,0 +1,76 @@
+# FETCHCONTENT_TRY_FIND_PACKAGE_MODE = OPT_IN, ALWAYS, NEVER
+# OPT_IN: FetchContent will only try to find the package if the user explicitly asks for it.
+# ALWAYS: FetchContent will always try to find the package.
+# NEVER: FetchContent will never try to find the package.
+
+include(FetchContent)
+
+## Dear ImGui
+FetchContent_Declare(
+    ImGui
+    GIT_REPOSITORY  https://github.com/ocornut/imgui.git
+    GIT_TAG         docking # or master
+    GIT_SHALLOW     TRUE
+    GIT_PROGRESS    TRUE
+    SOURCE_DIR      ${CMAKE_SOURCE_DIR}/third_party/imgui
+)
+FetchContent_MakeAvailable(ImGui)
+
+message(STATUS "📌 ImGui will be available at ${ImGui_SOURCE_DIR}")
+
+
+## Configurations
+
+# ---- Options ----
+option(IMGUI_BACKEND_GLFW_OPENGL3   "Use GLFW + OpenGL3 backend for ImGui" OFF)
+option(IMGUI_BACKEND_GLFW_VULKAN    "Use GLFW + Vulkan backend for ImGui" OFF)
+option(IMGUI_BACKEND_GLFW_WGPU      "Use GLFW + WebGPU backend for ImGui" OFF)
+
+set(IMGUI_DIR ${CMAKE_SOURCE_DIR}/third_party/imgui)
+
+include_directories(
+    ${IMGUI_DIR}
+    ${IMGUI_DIR}/backends
+)
+
+# ---- IMGUI Core ----
+set(IMGUI_SOURCES
+    ${IMGUI_DIR}/imgui.cpp
+    ${IMGUI_DIR}/imgui_draw.cpp
+    ${IMGUI_DIR}/imgui_widgets.cpp
+    ${IMGUI_DIR}/imgui_tables.cpp
+    # ${IMGUI_DIR}/imgui_demo.cpp
+)
+
+# ---- Backends ----
+if(IMGUI_BACKEND_GLFW_OPENGL3)
+    list(APPEND IMGUI_SOURCES
+        ${IMGUI_DIR}/backends/imgui_impl_glfw.cpp
+        ${IMGUI_DIR}/backends/imgui_impl_opengl3.cpp
+    )
+endif()
+
+if(IMGUI_BACKEND_GLFW_VULKAN)
+    list(APPEND IMGUI_SOURCES
+        ${IMGUI_DIR}/backends/imgui_impl_glfw.cpp
+        ${IMGUI_DIR}/backends/imgui_impl_vulkan.cpp
+    )
+
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -DVK_PROTOTYPES")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DVK_PROTOTYPES")
+endif()
+
+if(IMGUI_BACKEND_GLFW_WGPU)
+    list(APPEND IMGUI_SOURCES
+        ${IMGUI_DIR}/backends/imgui_impl_glfw.cpp
+        ${IMGUI_DIR}/backends/imgui_impl_wgpu.cpp
+    )
+
+    # WGPU Configuration
+    # ...
+endif()
+
+# ---- Print all options ----
+message(STATUS "IMGUI_BACKEND_GLFW_OPENGL3: ${IMGUI_BACKEND_GLFW_OPENGL3}")
+message(STATUS "IMGUI_BACKEND_GLFW_VULKAN:  ${IMGUI_BACKEND_GLFW_VULKAN}")
+message(STATUS "IMGUI_BACKEND_GLFW_WGPU:    ${IMGUI_BACKEND_GLFW_WGPU}")
