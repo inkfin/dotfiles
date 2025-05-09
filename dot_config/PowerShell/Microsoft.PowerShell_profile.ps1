07a41c4118e4db18623470a60e309b73139049a7
inkfin <inkfinite@qq.com>
2025-05-09 00:04:22 -0400

    fix(pwshrc): fix init problem

--- dot_config/PowerShell/Microsoft.PowerShell_profile.ps1
+++ dot_config/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -91,14 +91,6 @@ function rag {
     Remove-Item -Path $tmp
 }
 
-Invoke-Expression (&starship init powershell)
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
-
-Enable-TransientPrompt
-
-# imports
-Import-Module PSFzf
-
 # local bin
 Append-UserPath("$HOME\.local\bin")
 Append-UserPath("$HOME\.local\neovim\bin")
@@ -149,18 +141,25 @@ Init-EnvironmentVariable RUSTUP_HOME "$HOME\scoop\persist\rustup\.rustup"
 Init-EnvironmentVariable CARGO_HOME "$HOME\scoop\persist\rustup\.cargo"
 
 
+# Fzf
+Import-Module PSFzf
+
 . "$PSScriptRoot/PSReadLine_config.ps1"
 . "$PSScriptRoot/Utilities.ps1"
-
 
 # Post init
 
 # oh-my-posh
 #oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/wholespace.omp.json" | Invoke-Expression
+
 # starship
 function Invoke-Starship-TransientFunction {
   &starship module character
 }
+Invoke-Expression (&starship init powershell)
+Enable-TransientPrompt # after starship init
+# zoxide
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
 # scoop
-. "$($(Get-Item $(Get-Command scoop).Path).Directory.Parent.FullName)\apps\scoop-completion\current\add-profile-content.ps1"
+# . "$($(Get-Item $(Get-Command scoop).Path).Directory.Parent.FullName)\apps\scoop-completion\current\add-profile-content.ps1"
 Import-Module scoop-completion
