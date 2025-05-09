711cb19c42a9a2891d3e5169cc3c8cb63092f4f5
inkfin <inkfinite@qq.com>
2025-05-08 23:39:54 -0400

    fix(template cmake): fix template helper dependency

--- scripts/cmake/helpers.cmake
+++ scripts/cmake/helpers.cmake
@@ -2,21 +2,18 @@ function(set_binary_output_dirs TARGET OUTPUT_BASE)
     set(_outdir "${CMAKE_SOURCE_DIR}/${OUTPUT_BASE}")
 
     set_target_properties(${TARGET} PROPERTIES
-        RUNTIME_OUTPUT_DIRECTORY_RELEASE "${_outdir}/Release"
-        RUNTIME_OUTPUT_DIRECTORY_DEBUG   "${_outdir}/Debug"
+        ARCHIVE_OUTPUT_DIRECTORY "${_outdir}/$<CONFIG>"
+        LIBRARY_OUTPUT_DIRECTORY "${_outdir}/$<CONFIG>"
+        RUNTIME_OUTPUT_DIRECTORY "${_outdir}/$<CONFIG>"
     )
 
-    if(WIN32)
+    if(MSVC)
+        ## Visual Studio specific
         set_target_properties(${TARGET} PROPERTIES
-            VS_DEBUGGER_WORKING_DIRECTORY "${_outdir}/Debug"
+            VS_DEBUGGER_WORKING_DIRECTORY "$<TARGET_FILE_DIR:${TARGET}>"
+            VS_DEBUGGER_COMMAND           "$<TARGET_FILE:${TARGET}>"
         )
     endif()
 
-    if(CMAKE_BUILD_TYPE STREQUAL "Release")
-        set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${_outdir}/Release")
-    else()
-        set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${_outdir}/Debug")
-    endif()
-
-    message(STATUS "Set binary output directory for target ${TARGET} -> ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}")
+    message(STATUS "Set ${TARGET} binary output directory to ${_outdir}")
 endfunction()
