a52c57636c08df8b6d998fa7e0e33d1724e6a9f7
finkzhang <finkzhang@tencent.com>
2025-08-21 10:54:28 +0800

    update(nvim): add telescope ctags plug

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,67 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+local builtin = require("telescope.builtin")
+
+-- 自定义函数：列出光标下符号的 ctags 引用
+---@param use_cword boolean 是否使用光标下的单词
+local function ctags_refs(use_cword)
+    local symbol
+
+    if use_cword then
+        symbol = vim.fn.expand("<cword>")
+    else
+        symbol = vim.fn.input("Symbol: ")
+        if symbol == "" then
+            print("No symbol provided")
+            return
+        end
+    end
+
+    local tagsfile = "tags"
+    if vim.fn.filereadable(tagsfile) == 0 then
+        print("No tags file found")
+        return
+    end
+
+    -- grep tags 文件，找出 symbol 的引用 (kind:r)
+    local results = {}
+    for line in io.lines(tagsfile) do
+        if line:match("^" .. symbol .. "\t") and line:match("kind:r") then
+            table.insert(results, line)
+        end
+    end
+
+    if #results == 0 then
+        print("No references found for " .. symbol)
+        return
+    end
+
+    -- 调用 Telescope 展示结果
+    pickers
+        .new({}, {
+            prompt_title = "CTags Refs for " .. symbol,
+            finder = finders.new_table({
+                results = results,
+            }),
+            sorter = conf.generic_sorter({}),
+        })
+        :find()
+end
+
+-- 注册命令
+
+vim.api.nvim_create_user_command("CtagsRefs", function()
+    ctags_refs(true)
+end, {})
+
+vim.api.nvim_create_user_command("CtagsRefsInput", function()
+    ctags_refs(false)
+end, {})
+
+-- vim.keymap.set("n", "<leader>ft", ":CtagsRefs<CR>", { noremap = true, silent = true })
+-- vim.keymap.set("n", "<leader>fT", ":CtagsRefsInput<CR>", { noremap = true, silent = true })
+
 return {
     {
         "nvim-telescope/telescope.nvim",
@@ -22,6 +86,10 @@ return {
                     ".cache",
                 },
             },
+        },
+        keys = {
+            { "<leader>ft", "<cmd>CtagsRefs<CR>", desc = "CTags Refs" },
+            { "<leader>fT", "<cmd>CtagsRefsInput<CR>", desc = "CTags Refs Input" },
         },
     },
     {
