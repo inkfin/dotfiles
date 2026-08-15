500a58c33053467e8d024fc6cd66974792e8e730
inkfin <inkfinite@qq.com>
2026-08-15 13:23:12 +0800

    fix(proxy): use http for all_proxy

--- dot_config/PowerShell/Utilities.ps1
+++ dot_config/PowerShell/Utilities.ps1
@@ -75,10 +75,10 @@ function cmb  { cmake --build $args }
 function setproxy {
     $env:HTTP_PROXY  = "http://127.0.0.1:7890"
     $env:HTTPS_PROXY = "http://127.0.0.1:7890"
-    $env:ALL_PROXY   = "socks5://127.0.0.1:7890"
+    $env:ALL_PROXY   = "http://127.0.0.1:7890"
     $env:http_proxy  = "http://127.0.0.1:7890"
     $env:https_proxy = "http://127.0.0.1:7890"
-    $env:all_proxy   = "socks5://127.0.0.1:7890"
+    $env:all_proxy   = "http://127.0.0.1:7890"
     Write-Output "proxy on"
 }
 
--- dot_config/nushell/vendor/autoload/edit.nu
+++ dot_config/nushell/vendor/autoload/edit.nu
@@ -51,10 +51,10 @@ alias tksv = tmux kill-server
 def --env setproxy [] {
     $env.HTTP_PROXY = "http://127.0.0.1:7890"
     $env.HTTPS_PROXY = "http://127.0.0.1:7890"
-    $env.ALL_PROXY = "socks5://127.0.0.1:7890"
+    $env.ALL_PROXY = "http://127.0.0.1:7890"
     $env.http_proxy = "http://127.0.0.1:7890"
     $env.https_proxy = "http://127.0.0.1:7890"
-    $env.all_proxy = "socks5://127.0.0.1:7890"
+    $env.all_proxy = "http://127.0.0.1:7890"
 }
 
 def --env unsetproxy [] {
